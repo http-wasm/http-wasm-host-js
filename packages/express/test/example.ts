9edcc032d0f8b7985535bc72ef69b6640385a125
Anuraag Agrawal <anuraaga@gmail.com>
2022-10-14 16:03:26 +0900

    feat: implement ABI for router middleware

--- packages/express/test/example.ts
+++ packages/express/test/example.ts
@@ -15,13 +15,17 @@ const serveJson = (req: Request, res: Response) => {
   res.contentType('application/json').send(responseBody);
 };
 
-describe('auth middleware', async () => {
+const servePath = (req: Request, res: Response) => {
+  res.contentType('text/plain').send(req.url);
+};
+
+describe('auth middleware', async function () {
   const wasmPath = '../../testdata/examples/auth.wasm';
 
   let server: http.Server;
   let url: string;
 
-  before(async () => {
+  before(async function () {
     // Initialize the express server.
     const app = express();
 
@@ -33,7 +37,87 @@ describe('auth middleware', async () => {
     app.use(mw);
 
     // Register the real request handler.
-    app.get('/', serveJson);
+    app.get('/*', serveJson);
+
+    // Start the server and wait for it to be ready.
+    await new Promise((resolve) => {
+      server = app.listen(0, () => resolve(undefined));
+    });
+
+    const address = server.address() as AddressInfo;
+    url = `http://127.0.0.1:${address.port}`;
+  });
+
+  // Shutdown server after tests finish.
+  after(async function () {
+    return new Promise((resolve, reject) => {
+      server.close((err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(undefined);
+        }
+      });
+    });
+  });
+
+  it('no header', async function () {
+    const response = await fetch(url);
+    assert.equal(response.status, 401);
+    assert.equal(
+      response.headers.get('www-authenticate'),
+      'Basic realm="test"',
+    );
+  });
+
+  it('empty header', async function () {
+    const response = await fetch(url, {
+      headers: { Authorization: '' },
+    });
+    assert.equal(response.status, 401);
+    assert.equal(
+      response.headers.get('www-authenticate'),
+      'Basic realm="test"',
+    );
+  });
+
+  it('valid header', async function () {
+    const response = await fetch(url, {
+      headers: { Authorization: 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==' },
+    });
+    assert.equal(response.status, 200);
+    const content = await response.text();
+    assert.equal(content, responseBody);
+  });
+
+  it('invalid header', async function () {
+    const response = await fetch(url, {
+      headers: { Authorization: '0' },
+    });
+    assert.equal(response.status, 401);
+    assert.notExists(response.headers.get('www-authenticate'));
+  });
+});
+
+describe('router middleware', async function () {
+  const wasmPath = '../../testdata/examples/router.wasm';
+
+  let server: http.Server;
+  let url: string;
+
+  before(async function () {
+    // Initialize the express server.
+    const app = express();
+
+    // Configure and compile the WebAssembly guest binary. In this case,
+    // it is an auth interceptor.
+    const mw = await httpwasm({ wasmPath });
+
+    // Register the middleware with the express server.
+    app.use(mw);
+
+    // Register the real request handler.
+    app.get('/*', servePath);
 
     // Start the server and wait for it to be ready.
     await new Promise((resolve) => {
@@ -58,40 +142,27 @@ describe('auth middleware', async () => {
       }),
   );
 
-  it('no header', async () => {
-    const response = await fetch(url);
-    assert.equal(response.status, 401);
-    assert.equal(
-      response.headers.get('www-authenticate'),
-      'Basic realm="test"',
-    );
-  });
-
-  it('empty header', async () => {
-    const response = await fetch(url, {
-      headers: { Authorization: '' },
-    });
-    assert.equal(response.status, 401);
-    assert.equal(
-      response.headers.get('www-authenticate'),
-      'Basic realm="test"',
-    );
-  });
-
-  it('valid header', async () => {
-    const response = await fetch(url, {
-      headers: { Authorization: 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==' },
-    });
-    assert.equal(response.status, 200);
+  it('/', async function () {
+    const response = await fetch(`${url}${this.test!.title}`);
     const content = await response.text();
-    assert.equal(content, responseBody);
+    assert.equal(content, 'hello world');
   });
 
-  it('invalid header', async () => {
-    const response = await fetch(url, {
-      headers: { Authorization: '0' },
-    });
-    assert.equal(response.status, 401);
-    assert.notExists(response.headers.get('www-authenticate'));
+  it('/nothosst', async function () {
+    const response = await fetch(`${url}${this.test!.title}`);
+    const content = await response.text();
+    assert.equal(content, 'hello world');
+  });
+
+  it('/host/a', async function () {
+    const response = await fetch(`${url}${this.test!.title}`);
+    const content = await response.text();
+    assert.equal(content, '/a');
+  });
+
+  it('/host/b?name=panda', async function () {
+    const response = await fetch(`${url}${this.test!.title}`);
+    const content = await response.text();
+    assert.equal(content, '/b?name=panda');
   });
 });
